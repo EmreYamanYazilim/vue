af213dc5ceb3811585d2876340b9cc990ce57489
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-08 22:46:42 +0300

    İç İçe Bileşenler global

--- vue CLI/vue-playlist/src/App.vue
+++ vue CLI/vue-playlist/src/App.vue
@@ -1,19 +1,13 @@
 
 <template>
-  <p>{{ title }}</p>
-  <button v-on:click="baslikDegisir">değiştir</button>
-
+        <app-header></app-header>
 </template>
 
 <script>
 export default{
   data() {
     return {
-      title:"ilk Vue CLI çalışmam 10x hızında devam",
-    }
-  },methods: {
-    baslikDegisir:function  () {
-      this.title = "10x hızında Vue öğrenmeye devam";
+
     }
   },
 }
--- vue CLI/vue-playlist/src/main.js
+++ vue CLI/vue-playlist/src/main.js
@@ -1,5 +1,21 @@
 
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import Header from './header.vue'; 
+// import yanındaki ilk yazıdan adını Header olarak belirledim
+//  header.vueyi gösterebilmek için import ettik 
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// artık olaylarımızı bu arada yaparak global olarak işlemlerimizi yapacağız 
+// headervue importttan sonra app olarak component olarak göstermeliyoz
+app.component("app-header",Header) 
+// importaki ismi virgül sağına Header olarak virgülün solunda ilk belirttğimiz yerde header.vuede  app-header tagımız olacak 
+
+app.mount('#app')
