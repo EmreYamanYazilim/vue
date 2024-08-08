9693c8f4132d1677a42b7d3dcfc841474c940be9
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-08 20:13:20 +0300

    sıfırdan bir Vue

--- vue CLI/vue-playlist/src/App.vue
+++ vue CLI/vue-playlist/src/App.vue
@@ -1,47 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <p>{{ title }}</p>
+  <button v-on:click="baslikDegisir">değiştir</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default{
+  data() {
+    return {
+      title:"ilk Vue CLI çalışmam 10x hızında devam",
+    }
+  },methods: {
+    baslikDegisir:function  () {
+      this.title = "10x hızında Vue öğrenmeye devam";
+    }
+  },
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
