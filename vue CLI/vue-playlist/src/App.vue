ff7d74ce3930a925c6d6579a31354c84fa167146
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-09 11:02:18 +0300

    local deneme 2

--- vue CLI/vue-playlist/src/App.vue
+++ vue CLI/vue-playlist/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <app-header></app-header>
   <app-footer></app-footer>
+  <app-nav></app-nav>
 </template>
 
 <script>
 import Header from './header.vue'
 import Footer from './footer.vue'
+import Nav from './nav.vue';
 
 export default {
   components: {
     "app-header": Header,
-    "app-footer": Footer
+    "app-footer": Footer,
+    "app-nav": Nav
   },
   data() {
     return {
