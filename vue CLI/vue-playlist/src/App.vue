af60a8f0037aa8f99c4799556779c1c363220b15
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-12 19:55:27 +0300

    Dinamik bilesenler components

--- vue CLI/vue-playlist/src/App.vue
+++ vue CLI/vue-playlist/src/App.vue
@@ -1,76 +1,57 @@
 <template>
-  <h1>
-    Slot'lar üzerindek Css işlemleri
-  </h1>
+  <!-- <form-one></form-one>
+  <form-two></form-two> -->
+  <!-- böylelikle  alt-bilesen.vue içiden referans larak App vue içindeki kodlaromızı form.vueler içne atarak onların taglarını altta belitiğimiz üzere
+  import ederek  çağırıp dinamikleştirdik 
+  -->
 
-  <alt-bilesen>
-    asdasd
-    <template #form-header>
-      <h2>
-        Giriş Formu
-      </h2>
-      <p>Giriş yapabilmek için lütfen formu doldurun</p>
-    </template>
+  <!--  şimdi yukarıda görüldüğü gibi böyle gerekli olanı çağırmak için kalıp olan bir component tagımız var bunun içinde is diyerek cağırmak istediğimiz tagı yazıyoruz  aşağıdaki gibi -->
 
-    <template #form-elements>
-      <label  for="email">E-mail : </label>
-      <input type="email" name="email" placeholder="E posta Adersinizi Giriniz">
-
-      <label for="password">Şifre : </label>
-      <input type="password" name="password" id="" placeholder="Şifrenizi Giriniz">
-    </template>
-
-    <template #form-controls>
-      <button>Giriş Yap</button>
-    </template>
-  </alt-bilesen>
+<component is="form-one"></component>  <!-- diikkat  tag otomatik doldurken :is olarak iki noktalı yazıyor  2 noktayı  kaldırman gerek o noktanın amacı v-bind yada başka bişi önüne yazılabilir diye ototmatik geliyor  -->
 
 
 
+<!-- şimdi bunu dinamik hala getirelim alttaki gibi  -->
+ <!--  keep alive   yazılanları hafızada tutmaya yaruyor aşağıda  2 button ve 2 farklı sayfa oluşturduk tıkladığında o sayfalara gidicek 
+   keep alive sayesinde  biz  1. sayfada bişi yazıp 2. sayfaya tıkladığımızda  yazdığımız verilerin  kaybolmadan orda tutulmasını  sağlıyor
+   keep alive yazılmamış olsaydı button ile sayfa değiştirdiğimizde yazdıklarımız silinirdi
+   dinamik compomentimizi onun içinde tutuyoruz-->
+<keep-alive> 
+  <component v-bind:is="compGidis"></component> 
+ </keep-alive>
+<!-- v-bind yaparak return daki veriyi çekmek için  istekte bulunuyorz  böylelikle compGidisi dinamik yaptık  alttaki hangi buttona tıklarsak ordaki veriyi otoaiatk olarak 
+ yukarıdaki component içinde is içine bize verecek   is eşitliği bize -->
 
-  <h3 v-bind:class="$style.blue">App.vue içinde style ile  module ile css verme  </h3>
-  <h3 v-bind:class="xbilesen.green">Appvue içinde style ile  modüle takma isim vererek o takma isim ile  yakalayarak renk verme</h3>
+<button v-on:click="compGidis='form-one'">Form 1</button>
+<button v-on:click="compGidis='form-two'">Form 2</button>
+
 </template>
 
 <script>
-import altBilesen from './components/alt-bilesen.vue'
+import formOne from './components/form-one.vue'
+import formtwo from './components/form-two.vue'
 
 export default {
-  components:{
-    "alt-bilesen": altBilesen
+  components: {
+    "form-one": formOne,
+    "form-two": formtwo
   },
   data() {
     return {
-      
+        compGidis:'form-one',
+        compGidis:'form-two'
     }
   },
 }
 
 </script>
 
-<style scoped>
-:deep(input){
- /* sadece alt bileşendeki kodları etkileyen  Css içinde :deep Kullanoyoruz  aksi taktirde  App.vue ana Rotemiz olduğu için tüm sayfayı etkiler  */
-}
-:global(input){
-  /* scoped olsada olmasada  her yerdeki inputu istediğimiz Css'i verir   */
-}
+<style>
+
 </style>
 
-<style module>
-/* style içinde modüllerimiz var  bunu kullanabilmek için modül olarak belirtmemiz gerekecek 
-v-bind:class="$style.blue"  style içinde bir module olduğunu yakalamk için $style.blue diyerek bağlayarak veriyoruz */
-.blue{
-  color: blue;
-}
-</style>
 
-<style module="xbilesen">
-      /* v-bind:class = "xbilesen.green"   vererek önce  modüldeki takma ismi sonra içindeki cssi nokta ile birleştirerek veriyoruz*/
-  .green{
-    color: green;
-  }
-</style>
+
 
 
 
---
+++ vue CLI/vue-playlist/src/components/form-one.vue
@@ -0,0 +1,38 @@
+<template>
+    <alt-bilesen>
+        <template #form-header>
+            <h2>
+                Giriş Formu 1
+            </h2>
+            <p>Giriş yapabilmek için lütfen formu doldurun</p>
+        </template>
+
+        <template #form-elements>
+            <label for="email">E-mail : </label>
+            <input type="email" name="email" placeholder="E posta Adersinizi Giriniz">
+
+            <label for="password">Şifre : </label>
+            <input type="password" name="password" id="" placeholder="Şifrenizi Giriniz">
+        </template>
+
+        <template #form-controls>
+            <button>Giriş Yap</button>
+        </template>
+    </alt-bilesen>
+</template>
+
+<script>
+import altBilesen from './alt-bilesen.vue'
+
+export default {
+    components: {
+        "alt-bilesen": altBilesen
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
