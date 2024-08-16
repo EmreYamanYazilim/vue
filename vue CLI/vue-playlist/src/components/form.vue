6a0e091e33b866db38aaaf451c0dab758f928e35
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-16 19:59:25 +0300

    kullanıcı ekleme yazdırma

--- vue CLI/vue-playlist/src/components/form.vue
+++ vue CLI/vue-playlist/src/components/form.vue
@@ -1,17 +1,34 @@
 <template>
-  <form action="">
-    <input type="text" v-model="text"> <!-- v model ile 2 yönlü bağlanma v model ile aynı zamanda bind etmiş olyporuz ve değişkeni input içine yazdırabiliyoruz-->
-    <p>{{ text }}</p> <!-- inputta yazdıklarımız  p içindede yazılacak  direk value olarak alacak-->
-  </form>
+    <div>
+        <form action="#">
+            <input type="text" v-model="student" @keypress="addStudend">
+            <!-- v model ile 2 yönlü bağlanma v model ile aynı zamanda bind etmiş olyporuz ve değişkeni input içine yazdırabiliyoruz-->
+            <div v-for="student in studetns" :key="student"> <!-- yazılanları  yazdır-->
+                <span>{{student}}</span>
+            </div>
+        </form>
+    </div>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            text:'deneme',
+            student:'',
+            studetns:[]
         }
-    }
+    },
+    methods: {
+        addStudend(e){
+            if(e.key == "." && this.student){  //  . basarak ekle
+                if(!this.studetns.includes(this.student)){ // array  içinde bak student inputta yazdığımız yoksa işleme devam et
+                    this.studetns.push(this.student) // yoksa  students array içine student inputu içindekini ekle
+                }
+                this.student="" // inputu sıfırla
+            }
+        }
+    },
 }
 </script>
 
