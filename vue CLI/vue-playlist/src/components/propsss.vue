037335308cfe29b29796608c6cbc9e03b4dc6365
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-16 16:37:32 +0300

    propsss

---
+++ vue CLI/vue-playlist/src/components/propsss.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{ deneme }}</h2>
+        <p>{{ deneme2 }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['deneme','deneme2'],  // bind edilen değişkenler burada tanımlanarak yukarıda kullanılmaya olanak tanır 
+data() {
+    return {
+        
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
