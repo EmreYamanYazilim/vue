ab67e67fc9a2324b07cecc44fea8b7b624726bad
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-16 20:34:53 +0300

    array içi Delete işlemleri

--- vue CLI/vue-playlist/src/components/form.vue
+++ vue CLI/vue-playlist/src/components/form.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <form action="#">
-            <input type="text" v-model="student" @keypress="addStudend">
+            <input type="text" v-model="student" @keyup="addStudend">
             <!-- v model ile 2 yönlü bağlanma v model ile aynı zamanda bind etmiş olyporuz ve değişkeni input içine yazdırabiliyoruz-->
             <div v-for="student in studetns" :key="student"> <!-- yazılanları  yazdır-->
-                <span>{{student}}</span>
+                <span @click="studentDelete(student)">{{ student }}</span>
             </div>
         </form>
     </div>
@@ -15,23 +15,26 @@
 export default {
     data() {
         return {
-            student:'',
-            studetns:[]
+            student: '',
+            studetns: []
         }
     },
     methods: {
-        addStudend(e){
-            if(e.key == "." && this.student){  //  . basarak ekle
-                if(!this.studetns.includes(this.student)){ // array  içinde bak student inputta yazdığımız yoksa işleme devam et
+        addStudend(e) {
+            if (e.key == "." && this.student) {  //  . basarak ekle
+                if (!this.studetns.includes(this.student)) { // array  içinde bak student inputta yazdığımız yoksa işleme devam et
                     this.studetns.push(this.student) // yoksa  students array içine student inputu içindekini ekle
                 }
-                this.student="" // inputu sıfırla
+                this.student = "" // inputu sıfırla
             }
+        },
+        
+        
+        studentDelete(studentx) {
+            this.studetns.splice(this.studetns.indexOf(studentx), 1)
         }
     },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
