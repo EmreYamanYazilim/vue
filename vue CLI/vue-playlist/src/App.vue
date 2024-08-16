b9b233f71c843ed60e84de8b240daf97f65ac8ac
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-16 17:02:34 +0300

    componentler arası event Emit

--- vue CLI/vue-playlist/src/App.vue
+++ vue CLI/vue-playlist/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <cea  @cleann="temizle">asd</cea> <!-- yeni oluşturduğum tag ile cleannn eventi oluşturdum bu eventi biz cea.vue içinden emit ile fırlatarak yakaladım  ve temizle adını verdim App.vue içinde moethods içinde
+    bu onu yeni bir fn olarak tanımlayaraak refs vererek ref icerigi yakaladım ve silmesini sağladık-->
+    <input type="text" ref="icerik">
+
+
     <Propsss :deneme="title1" :deneme2="title2"></Propsss>
     <app-headerIki></app-headerIki>
     <router-view></router-view>
@@ -16,6 +21,7 @@ import AddBlog from './components/show-blog.vue'
 import HeaderIki from './components/headerheader.vue'
 import pratikBilgiler from "./components/pratik-bilgiler.vue"
 import Propsss from "./components/propsss.vue"
+import cea from './components/component-event-aktarim.vue'
 
 export default {
   components: {
@@ -23,12 +29,18 @@ export default {
     "add-blog": AddBlog,
     "app-headerIki": HeaderIki,
     "pratik-bilgiler": pratikBilgiler,
-    Propsss // direk olarak  components içinden imiport ettiğimiz  adı verebiliriz takma at yukarıdakiler gibi bu direk o form öncesindeki takma adımızla yukarıda tag açabiliyoruz
+    Propsss, // direk olarak  components içinden imiport ettiğimiz  adı verebiliriz takma at yukarıdakiler gibi bu direk o form öncesindeki takma adımızla yukarıda tag açabiliyoruz
+    cea
   },
   data() {
     return {
       title1: "üst tarafa veri çekmek için kullanacağım ",
       title2:"burdai değişkeni üst tarafa yani propsss.vue içine veri çekmek ve kullanmak için  burdaki propsss tagı içinde bind sonrası değişken adımızıbelirttik= içinede burdaki değişkenimizi vererek bağladık  bind sonrası oluşan bu değişken adını propss.vue  içinde props:[] içinde tanımlayarak yakaladık ve o  componenttede burdan gelen değişkeni kullanabildik "
+    }
+  },
+  methods: {
+    temizle() {
+      this.$refs.icerik.value="" // refi içerik olan inputu boşaltmak için 
     }
   },
 }
---
+++ vue CLI/vue-playlist/src/components/component-event-aktarim.vue
@@ -0,0 +1,43 @@
+<template>
+    <div @click="$emit('cleann')"> <!--emit ile bir event oluşturduk bu artık @clik gibi App.vue içinde bize tıklanma sağlayacak  div içindeki sil buttonuna tıkladığımızda çalışacak
+        app.vue içindeki diğer parametrelerle beraber 
+
+    <cea  @cleann="temizle">asd</cea> 
+    
+    yeni oluşturduğum tag ile cleannn eventi oluşturdum bu eventi biz cea.vue içinden emit ile fırlatarak yakaladım  ve temizle adını verdim App.vue içinde moethods içinde
+    bu onu yeni bir fn olarak tanımlayaraak refs vererek ref icerigi yakaladım ve silmesini sağladık
+    <input type="text" ref="icerik">
+
+        altında  temizle fn oluştararak  cea dışındaki app.vueye ait inputun içindekileri sildirmek içinde kullandım
+
+        methods: {
+    temizle() {
+      this.$refs.icerik.value="" // refi içerik olan inputu boşaltmak için 
+    }
+  },
+  
+  
+  
+  -->
+        <!-- kısa yolu  emiti yakalamk için  alttan methods vererek işlemi yapmak zorunda kalırdık-->
+        <button>sil</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
