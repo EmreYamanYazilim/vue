1a057f80ad361bfb1b163b712cc2f6a1adac7a55
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-17 17:28:11 +0300

    Router go & push

--- btk/route-app/src/App.vue
+++ btk/route-app/src/App.vue
@@ -18,13 +18,19 @@ import HelloWorld from './components/HelloWorld.vue'
           <!-- attaki  örnek gibi routerlinklerimizideki indexjs içindeki route bölümündeki  namelerden yakalayarak kullanmak için ile  bind etmemiz gerekyior başlarına : koymamız kafi ve içlerine bind ettiğimiz için {name:'home'} şeklinde yine aynı linki çekebiliriz  -->
           <RouterLink :to="{ name: 'home' }">Home</RouterLink>
           <RouterLink :to="{ name: 'about' }">About</RouterLink>
-          
+
           <!-- yukarıdaki gibi routerlinklerimizdeki indexjs içindeki route bölümünde routelerinden yakalyarak verebiliriz bunun için yine bind ederek name yerine path koyacağız -->
           <!-- <RouterLink :to="{ path: '/' }">Home</RouterLink>
           <RouterLink :to="{ path: '/about' }">About</RouterLink> -->
 
           <router-link :to="{ name: 'studentx' }">Öğrenciler</router-link>
         </nav>
+<div>
+  <span @click="anasayfa">Anasayfa</span>
+  <span @click="geri">geri</span>
+  <span @click="ileri">ileri</span>
+</div>
+
       </div>
     </header>
 
@@ -32,7 +38,28 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    anasayfa(){
+      this.$router.push({name:'home'}) // yukarıdaki click eventleri ile  ana sayfaya direk yönlendireceğiz
+    },
+    geri(){
+      this.$router.go(-1) // go -1 ile geri gecicek
+    },
+    ileri(){
+      this.$router.go(1) // ileri sayfaya gidecek
+    }
+  },
+}
+</script>
+
 <style scoped>
+span{
+  padding: 20px 10px;
+  cursor: pointer;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
