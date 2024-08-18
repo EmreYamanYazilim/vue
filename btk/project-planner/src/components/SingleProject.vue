f9d8e065317f26b41bea77afc7f5538970730f89
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-18 11:11:32 +0300

    icons

--- btk/project-planner/src/components/SingleProject.vue
+++ btk/project-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
     <div id="project">
         <div class="actions">
             <h2 @click="showDetails = !showDetails">{{ project.title }}</h2>
+            <div class="icons">
+                <span class="material-icons">edit</span>
+                <span class="material-icons">delete</span>
+                <span class="material-icons">check</span>
+            </div>
         </div>
         <div v-if="showDetails" class="details">
             <p>{{ project.details }}</p>
@@ -14,7 +19,7 @@ export default {
     props: ['project'],
     data() {
         return {
-            showDetails:false
+            showDetails: false
         }
     },
 }
@@ -27,10 +32,24 @@ export default {
     padding: 10px 20px;
     border-radius: 4px;
     box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-    border-left: 4px solid #e90074;
+    border-left: 4px solid rgb(243, 88, 32);
 }
 
 h2 {
     cursor: pointer;
+}
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+.material-icons:hover {
+    color: #616060;
 }
 </style>
