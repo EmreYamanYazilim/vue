0a701b0f42a39adc9e80fd14159034d69dc59d81
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-20 21:32:38 +0300

    Filter nav component

---
+++ btk/project-planner/src/components/FilterNav.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="filter-nav">
+        <button @click="updateFilter('all')" :class="{ active: current === 'all' }">all</button>
+        <button @click="updateFilter('completed')" :class="{active: current === 'completed'}">Tamamalanan</button>
+        <button @click="updateFilter('ongoing')" :class="{active:current === 'ongoing'}">ongoing</button>
+
+    </nav>
+</template>
+
+<script>
+
+export default {
+    props: ['current'],
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style></style>
