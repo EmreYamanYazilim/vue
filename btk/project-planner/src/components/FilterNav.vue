37bcd641324db321eb681875aa9378ec7ff1cf37
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-20 21:39:03 +0300

    style

--- btk/project-planner/src/components/FilterNav.vue
+++ btk/project-planner/src/components/FilterNav.vue
@@ -11,12 +11,30 @@
 
 export default {
     props: ['current'],
-    data() {
-        return {
-            
+    methods: {
+        updateFilter(by) {
+            this.$emit('filterChange', by)
         }
     },
 }
 </script>
 
-<style></style>
+<style>
+
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+
+</style>
