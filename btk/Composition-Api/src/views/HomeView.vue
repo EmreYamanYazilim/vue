358fa40315db81e47953b429bd11b90ecf5e9028
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-22 08:09:48 +0300

    Setup

--- btk/Composition-Api/src/views/HomeView.vue
+++ btk/Composition-Api/src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
+<template>
+<div>
+  <p>{{ name }} - {{ surname }}</p>
+  <button @click="clicked">Click</button>
+
+</div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    let name = "emre";
+    let surname = "Yaman";
+    // aynı şekilde bir methodda kullanabilioruz  const olarak methodu belirleyerek 
+    const clicked = () => {
+      console.log("clicked");      
+    }
+    return{name,surname,clicked} //  constun altında olacak   değişkenlerimöizi burdan alarak yukarıda gösterebiliyoruz 
+// data return yerine setup içinde değişkenlerimizi belirterek kullanabiliyoruz
+  }
+}
 
 </script>
-
-<template>
-  <main>
-    HOME
-  </main>
-</template>
