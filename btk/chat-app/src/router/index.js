56f2babeb55847dd9f8b37347c7a360668e3bf9f
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-26 16:39:11 +0300

    Chat-app kurulum düzenleme

---
+++ btk/chat-app/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '@/views/Welcome.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: Welcome
+    },
+    {
+      path: '/chatroom',
+      name: 'chatroom',
+      component: () => import('../views/Chatroom.vue')
+    }
+  ]
+})
+
+export default router
