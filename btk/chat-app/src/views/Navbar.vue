c9e9187c47774ef11773cd8f787ace9c52016d35
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-08-28 20:51:01 +0300

    Logout composable useLogout.js ile çıkş işlemleri

--- btk/chat-app/src/views/Navbar.vue
+++ btk/chat-app/src/views/Navbar.vue
@@ -5,14 +5,26 @@
                 <p>Merhaba 'DisplayName'</p>
                 <p class="email">şu anda 'Email' ile oturum açıldı</p>
             </div>
-            <button>Çıkış</button>
+            <button class="pointer" @click="handleClick">Çıkış</button>
 
         </nav>
     </div>
 </template>
 
 <script>
+import useLogout from '@/composables/useLogout';
 export default {
+    setup() {
+        const { error, logout } = useLogout();
+        const handleClick = async () => {
+            await logout();
+            if (!error.value) {
+                console.log('güvenli çıkış yapıldı');
+                
+            }
+        }
+        return { error, handleClick }
+    }
 
 }
 </script>
@@ -36,5 +48,8 @@ nav p {
 nav p.email {
     font-size: 14px;
     color: #888;
+}
+.pointer{
+    cursor: pointer;
 }
 </style>
